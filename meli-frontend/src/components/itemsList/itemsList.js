043f7c7477b72165182cedc1a2178b0039550419
SoriaJulia <soriajulias@gmail.com>
2021-10-02 00:06:32 -0300

    estilos y bootstrap

--- meli-frontend/src/components/itemsList/itemsList.js
+++ meli-frontend/src/components/itemsList/itemsList.js
@@ -1,13 +1,44 @@
+import './itemsList.scss';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/esm/Col';
+import Image from 'react-bootstrap/Image'
+
 export default function ItemsList(props){
     const {items} = props;
 
     return (
-        items.map(item => {
-            return (
-                <div key={item.id}>
-                    {item.title}
-                </div>
-            )
-        })
+        <Container>
+            {
+                items.map(item => {
+                    return (
+                        <Row key={item.id}>
+                            <Col xs={{span:2, offset:1}} >
+                                <Image 
+                                src={item.thumbnail} 
+                                alt={item.title}
+                                width="180px" 
+                                height="180px" 
+                                className='item image'/>
+                            </Col>
+                            <Col xs={6}>
+                                <div className='item price'>
+                                    {item.price}
+                                </div>
+                                <div className='item title'>
+                                    {item.title}
+                                </div>
+                            </Col>
+                            <Col xs={2}>
+                                <div className='item state'>
+                                    {item.address.state_name}
+                                </div>
+                            </Col>
+                            <hr/>
+                        </Row>
+                    )
+                })
+            }
+        </Container>
     )
 }
--- meli-frontend/src/components/search/search.js
+++ meli-frontend/src/components/search/search.js
@@ -1,29 +1,56 @@
+import'./search.scss'
 import{useEffect, useState} from "react"
 import useAsync from "../../hooks/useAsync";
+import Navbar from "react-bootstrap/Navbar";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Container from 'react-bootstrap/esm/Container';
+
 function searchItem(searchQuery) {
     return fetch(`http://localhost:3000/api/items?q=${searchQuery}`).then((value)=>{return value.json()})
   }
+
 export default function Search(props){
-    const [inputVal, setInputVal] = useState("");
-    const {error, status, data, run} = useAsync();
-    const {setItemsList} = props;
+  const [inputVal, setInputVal] = useState("");
+  const {error, status, data, run} = useAsync();
+  const {setItemsList} = props;
 
-    useEffect(()=>{
-        if(status === "resolved") {
-            setItemsList(data);
-        }
-    },[setItemsList, data, status]);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        run(searchItem(inputVal));
+  useEffect(()=>{
+      if(status === "resolved") {
+          setItemsList(data);
       }
-    return (
-        <>
-        <form onSubmit={handleSubmit} >
-        <input value={inputVal} onChange={(e)=>setInputVal(e.target.value)} type="text"/>
-        <button type="submit">buscar</button>
-      </form>
-        </>
+  },[setItemsList, data, status]);
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      run(searchItem(inputVal));
+    }
+
+  return (
+      <Navbar>
+        <Container>
+          <div className="col-1 offset-1 d-inline">
+            <img 
+              src="/Logo_ML.png"
+              srcSet="/Logo_ML@2x.png.png"
+              alt="Logo Mercado Libre"
+              height='40px'
+            />
+          </div>
+          <Form onSubmit={handleSubmit} className='d-inline col-9 mr-2'>
+            <InputGroup>
+              <Form.Control       
+                placeholder="Nunca dejes de buscar"
+                className="mr-2"
+                aria-label="Search"
+                value={inputVal} 
+                onChange={(e)=>setInputVal(e.target.value)}
+                />
+              <Button variant="secondary">Search</Button>
+            </InputGroup>
+          </Form>    
+        </Container>
+      </Navbar>
     )
 }
